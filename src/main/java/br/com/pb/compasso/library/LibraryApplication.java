049aa1658096b6c821d4f65d9002773b33558980
MateusOK <mateusribeiro-2004@hotmail.com>
2023-07-11 17:22:00 -0300

    feat: initial commit

---
+++ src/main/java/br/com/pb/compasso/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.pb.compasso.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}

045f89bf218c35c825a69c1903a395a660e434b1
MateusOK <mateusribeiro-2004@hotmail.com>
2023-07-11 21:54:49 -0300

    feat: posting endpoints

--- src/main/java/br/com/pb/compasso/library/controller/BookController.java
+++ src/main/java/br/com/pb/compasso/library/controller/BookController.java
@@ -1,9 +1,11 @@
 package br.com.pb.compasso.library.controller;
 
+import br.com.pb.compasso.library.domain.entity.Book;
 import br.com.pb.compasso.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -16,4 +18,13 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @PostMapping("api/books")
+    public Book save(@RequestBody Book book){
+        return bookService.save(book);
+    }
+
+    @PostMapping("api/books/batch")
+    public List<Book> saveBooksBatch(@RequestBody List<Book> books) {
+        return bookService.saveAll(books);
+    }
 }

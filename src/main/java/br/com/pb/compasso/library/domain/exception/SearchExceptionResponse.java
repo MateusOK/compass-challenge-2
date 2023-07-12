5ec87cee8815789198144cfce46a1955fd5fb36b
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2023-07-12 12:51:38 -0300

    feat: create TypeMismatchException class

---
+++ src/main/java/br/com/pb/compasso/library/domain/exception/SearchExceptionResponse.java
@@ -0,0 +1,9 @@
+package br.com.pb.compasso.library.domain.exception;
+
+import org.springframework.http.ResponseEntity;
+
+public class TypeMismatchExceptionResponse{
+
+    private 
+
+}
---
+++ src/main/java/br/com/pb/compasso/library/domain/exception/TypeMismatchException.java
@@ -0,0 +1,2 @@
+package br.com.pb.compasso.library.domain.exception;public class TypeMismatchException {
+}

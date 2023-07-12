4e042339d5f551dc2722f238448564dec6a59688
MateusOK <mateusribeiro-2004@hotmail.com>
2023-07-11 21:53:58 -0300

    feat: save methods for posting

--- src/main/java/br/com/pb/compasso/library/model/impl/BookServiceImpl.java
+++ src/main/java/br/com/pb/compasso/library/model/impl/BookServiceImpl.java
@@ -1,15 +1,30 @@
 package br.com.pb.compasso.library.model.impl;
 
+import br.com.pb.compasso.library.domain.entity.Book;
 import br.com.pb.compasso.library.model.BookRepository;
 import br.com.pb.compasso.library.service.BookService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookServiceImpl implements BookService {
 
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookRepositoryImpl bookRepositoryImpl;
 
-    public BookServiceImpl(BookRepository bookRepository) {
+    public BookServiceImpl(BookRepository bookRepository, BookRepositoryImpl bookRepositoryImpl) {
         this.bookRepository = bookRepository;
+        this.bookRepositoryImpl = bookRepositoryImpl;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> saveAll(List<Book> books) {
+        return bookRepositoryImpl.saveAll(books);
     }
 }
--- src/main/java/br/com/pb/compasso/library/service/BookService.java
+++ src/main/java/br/com/pb/compasso/library/service/BookService.java
@@ -1,5 +1,13 @@
 package br.com.pb.compasso.library.service;
 
+import br.com.pb.compasso.library.domain.entity.Book;
+
+import java.util.List;
+
 public interface BookService {
+
+    Book save(Book book);
+
+    List<Book> saveAll(List<Book> books);
 
 }

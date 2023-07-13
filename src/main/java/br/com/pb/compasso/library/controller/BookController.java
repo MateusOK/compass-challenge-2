ce92d79cddaa1e5fe86b8637b53a7b34d3244c65
MateusOK <mateusribeiro-2004@hotmail.com>
2023-07-13 20:28:08 -0300

    refactor: add lombok annotation for constructor

--- src/main/java/br/com/pb/compasso/library/controller/BookController.java
+++ src/main/java/br/com/pb/compasso/library/controller/BookController.java
@@ -1,19 +1,14 @@
 package br.com.pb.compasso.library.controller;
 
 import br.com.pb.compasso.library.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class BookController {
 
-    private BookService bookService;
-
-    @Autowired
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
+    private final BookService bookService;
 
 }

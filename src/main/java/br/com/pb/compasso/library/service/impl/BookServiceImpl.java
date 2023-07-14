0b79d16fda6f455c8b6acec32282568f46a9b8a3
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2023-07-14 16:41:13 -0300

    fix: Exception treatment in get methods

--- src/main/java/br/com/pb/compasso/library/service/impl/BookServiceImpl.java
+++ src/main/java/br/com/pb/compasso/library/service/impl/BookServiceImpl.java
@@ -3,10 +3,19 @@ package br.com.pb.compasso.library.service.impl;
 import br.com.pb.compasso.library.dto.response.BookResponseDto;
 import br.com.pb.compasso.library.entity.Book;
 import br.com.pb.compasso.library.dto.request.BookResquestDto;
+import br.com.pb.compasso.library.exception.BadRequestException;
+import br.com.pb.compasso.library.exception.InternalServerException;
+import br.com.pb.compasso.library.exception.MethodArgumentNotValidCustomException;
+import br.com.pb.compasso.library.exception.PageNotFoundException;
 import br.com.pb.compasso.library.repository.BookRepository;
 import br.com.pb.compasso.library.service.BookService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import java.util.List;
 
 import java.util.ArrayList;
@@ -19,12 +28,14 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     @Override
+    @Validated
     public BookResponseDto saveBook(BookResquestDto request) {
         var response = bookRepository.save(new Book(request));
         return new BookResponseDto(response);
     }
 
     @Override
+    @Validated
     public List<BookResponseDto> saveMultipleBooks(List<BookResquestDto> request) {
         List<Book> books = request.stream()
                 .map(Book::new)
@@ -47,7 +58,7 @@ public class BookServiceImpl implements BookService {
                     book.getReleaseDate(), book.getPages(), book.getRating(), book.getGenre());
         }
         else {
-            throw new RuntimeException("book id not found" + id);
+            throw new InternalServerException("Book ID not found - " + id);
         }
     }
 
@@ -56,6 +67,9 @@ public class BookServiceImpl implements BookService {
         var response = bookRepository.findAll();
         var books = new ArrayList<BookResponseDto>();
         response.forEach(book -> books.add(new BookResponseDto(book)));
+        if(response.isEmpty()){
+            throw new PageNotFoundException("List of books is empty!");
+        }
         return books;
     }
 
@@ -64,6 +78,9 @@ public class BookServiceImpl implements BookService {
         var response = bookRepository.findByGenre(genre);
         var books = new ArrayList<BookResponseDto>();
         response.forEach(book -> books.add(new BookResponseDto(book)));
+        if(books.isEmpty()){
+            throw new BadRequestException("There is no books with this genre - " + genre);
+        }
         return books;
     }
 
@@ -72,6 +89,9 @@ public class BookServiceImpl implements BookService {
         var response = bookRepository.findByAuthor(author);
         var books = new ArrayList<BookResponseDto>();
         response.forEach(book -> books.add(new BookResponseDto(book)));
+        if(books.isEmpty()){
+            throw new BadRequestException("There is no books with this author - " + author);
+        }
         return books;
     }
 }

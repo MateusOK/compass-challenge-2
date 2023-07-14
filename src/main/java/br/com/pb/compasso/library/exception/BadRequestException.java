72a1ab4781704ad24b039a84426dd2b31fdb09f8
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2023-07-14 16:03:57 -0300

    feat: add new methodArgumentNotValidException class

---
+++ src/main/java/br/com/pb/compasso/library/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package br.com.pb.compasso.library.exception;
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/br/com/pb/compasso/library/exception/BookRestExceptionHandler.java
+++ src/main/java/br/com/pb/compasso/library/exception/BookRestExceptionHandler.java
@@ -4,23 +4,45 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class BookRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BookRestExceptionHandler.class);
 
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponseBody> handleTypeMismatchException(HttpServletRequest request, TypeMismatchException exc){
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ExceptionResponseBody error = new ExceptionResponseBody();
+
+        error.setTimeStamp(new Date());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.addError("Books parameters can't be null");
+        error.setPath(request.getContextPath());
+        LOGGER.error(ex.getMessage(), ex);
+
+        return new ResponseEntity<>(error, status);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<ExceptionResponseBody> handleTypeMismatchException(HttpServletRequest request, BadRequestException exc){
 
         ExceptionResponseBody error = new ExceptionResponseBody();
 
@@ -33,7 +55,9 @@ public class BookRestExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(PageNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
     public ResponseEntity<ExceptionResponseBody> handlePageNotFoundException(HttpServletRequest request, PageNotFoundException exc){
 
         ExceptionResponseBody error = new ExceptionResponseBody();
@@ -48,13 +72,18 @@ public class BookRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(InternalServerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
     public ResponseEntity<ExceptionResponseBody> handleInternalServerException(HttpServletRequest request, InternalServerException exc){
 
         ExceptionResponseBody error = new ExceptionResponseBody();
 
-
-
+        error.setTimeStamp(new Date());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.addError(exc.getMessage());
+        error.setPath(request.getServletPath());
+        LOGGER.error(exc.getMessage(), exc);
 
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
